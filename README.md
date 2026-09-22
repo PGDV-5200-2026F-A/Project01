f5bbfbfbbf931f7cae4c17a5050da47f99da0f59
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-09-22 15:33:31 -0400

    filter row content. comments.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Some helper JavaScript/Python utilities
 
-- `fetch-all-js`: fetch all objects from a given query and prepare a JSON for download.
+- `fetch-all-js`: fetch all objects from a given query, only keep objects that have a valid image, and save some of the objects's fields to a JSON.
 - `fetch-all-py`: fetch all objects from a given query and prepare a JSON for download.
--- fetch-all-js/main.js
+++ fetch-all-js/main.js
@@ -1,14 +1,25 @@
 const SI_URL = "https://api.si.edu/openaccess/api/v1.0/search";
+
+// how many rows to fetch per fetch request
 const OBJS_PER_QUERY = 1000;
 
 const QUERY_TERM = "astrolabe";
+
+// list to store objects
 const objects = [];
 
+// Wait for html to be available
 document.addEventListener("DOMContentLoaded", async () => {
+  // our button that starts the query
   const startButton = document.querySelector("#start-button");
+
+  // on click
   startButton.addEventListener("click", async () => {
     startButton.style.display = "none";
 
+    // query parameters as an object.
+    // easier to read and manipulate.
+    // initially going to fetch 0 rows, just to get a total row count
     const params = {
       q: `${QUERY_TERM} AND online_media`,
       api_key: SI_KEY,
@@ -16,41 +27,65 @@ document.addEventListener("DOMContentLoaded", async () => {
       sort: "id",
       rows: 0
     };
+
+    // this turns the object into text that can be appended to the url
     const paramString = new URLSearchParams(params).toString();
+
+    // fetch and decode response
     const res = await fetch(`${SI_URL}?${paramString}`);
     const data = await res.json();
 
+    // row count
     const totalRows = data["response"]["rowCount"];
 
+    // fetch in groups of 1000 rows (can be changed above)
     for (let qcnt = 0; qcnt < totalRows / OBJS_PER_QUERY; qcnt += 1) {
+      // update parameters to get results starting at rows 0, 1000, 2000, etc
       params.start = qcnt * OBJS_PER_QUERY;
       params.rows = OBJS_PER_QUERY;
       const paramString = new URLSearchParams(params).toString();
+
+      // fetch and decode
       const res = await fetch(`${SI_URL}?${paramString}`);
       const data = await res.json();
-      
-      for (let rcnt = 0; rcnt < data.response.rows.length; rcnt += 1) {
-        const row = data.response.rows[rcnt];
-        const ocnt = qcnt * OBJS_PER_QUERY + rcnt;
-        if (ocnt % 100 == 0) console.log(ocnt, "/", totalRows);
-        if (ocnt == 100) {
+
+      // iterate through rows
+      for (const row of data.response.rows) {
+
+        // object count
+        const ocnt = objects.length;
+
+        // print progress every 25 objects
+        if (ocnt % 25 == 0) console.log(ocnt, "/", totalRows);
+
+        // show save button after 32 objects have been added to list
+        if (ocnt == 32) {
           const b = document.createElement("button");
           b.innerHTML = "save json";
           document.body.appendChild(b);
           b.addEventListener("click", () => saveJSON(objects));
         }
 
+        // the first media item of this row
         rowMedia = row.content.descriptiveNonRepeating.online_media.media[0];
+
+        // if it has a thumbnail, check if it's a valid url
         if ("thumbnail" in rowMedia) {
           const validImg = await checkImageExists(rowMedia.thumbnail);
           if (validImg) {
-            objects.push(row);
-          } else {
-            console.log("no img ");
+            // if image is valid push some of the row's data to our objects list
+            // these are the fields we're interested in. might need to be adapted
+            const toSave = {
+              id: row.id,
+              url: row.url,
+              source: row.content.descriptiveNonRepeating.data_source ?? "",
+              description: row.content.freetext?.notes?.[0]?.content ?? "",
+              first_image: row.content.descriptiveNonRepeating.online_media.media[0]
+            }
+            objects.push(toSave);
           }
-        } else {
-          console.log("no thumb");
         }
+        // wait a bit to not get kicked out of SI's API
         await sleep(50);
       }
     }
